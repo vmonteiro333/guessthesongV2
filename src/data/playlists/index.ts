144c00c9870b3a7b1cf7721626cbeb6ec65d887f
vmonteiro333 <vitor.bouca.monteiro@gmail.com>
2026-09-25 12:37:54 -0300

    +playlists

--- src/data/playlists/index.ts
+++ src/data/playlists/index.ts
@@ -3,9 +3,13 @@ import type { Playlist } from "../../types/playlist";
 // GERADO AUTOMATICAMENTE pelos scripts de importação — evite editar à mão.
 
 import { playlist as playlist_playlist_bala } from "./playlist-bala";
+import { playlist as playlist_playlist_mourisco } from "./playlist-mourisco";
+import { playlist as playlist_playlist_pernocas } from "./playlist-pernocas";
 import { playlist as playlist_principal } from "./principal";
 
 export const playlists: Playlist[] = [
   playlist_playlist_bala,
+  playlist_playlist_mourisco,
+  playlist_playlist_pernocas,
   playlist_principal,
 ];
